f141852708b66e28737300bf5fc8b06ed5a919e7
chenzhibin <chenzb961@163.com>
2023-05-13 22:51:48 +0800

    feat:数据库参数修改

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   port: process.env.NODE_ENV === "production" ? 1026 : 1066, //监听端口号
-  db_host: process.env.NODE_ENV === "production" ? "18.166.17.194" : "127.0.0.1", //数据库地址
+  db_host: process.env.NODE_ENV === "production" ? "18.166.17.194" : "18.166.17.194", //数据库地址
   db_name: process.env.NODE_ENV === "production" ? "cz_db" : "test_cz_db", //数据库名称
-  db_account: process.env.NODE_ENV === "production" ? "cz_db" : "root", //数据库账户
-  db_password: process.env.NODE_ENV === "production" ? "123456" : "123456", //数据库密码
+  db_account: process.env.NODE_ENV === "production" ? "cz_db" : "test_cz_db", //数据库账户
+  db_password: process.env.NODE_ENV === "production" ? "zeF6BzmEXynTMszn" : "MeGsxNHAP4aDdy4e", //数据库密码
 };
